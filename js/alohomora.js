723714bbccec198a38d246592a5cf15e41db72ee
auroros <awumstead@gmail.com>
2018-08-15 16:35:17 -0400

    test

--- js/alohomora.js
+++ js/alohomora.js
@@ -3,7 +3,13 @@ var btns = document.querySelectorAll(".btn");
 for (var i = 0; i < btns.length; i++) {
     /* do it this way, instead of via forEach, for IE compatibility */
     btns[i].addEventListener("click", function (evt) {
-        mouseCheck(); /* this comes from mouseDetector.js */    
+        mouseCheck(); /* this comes from mouseDetector.js */
+        ga('send', {
+            hitType: 'event',
+            eventCategory: 'Videos',
+            eventAction: 'play',
+            eventLabel: 'Fall Campaign'
+        });
     });
 }
 /* perform user agent detection and return numerical and textual values*/
@@ -11,32 +17,30 @@ function detectPlatform() {
     if (navigator.userAgent.match(/iPhone/) || navigator.userAgent.match(/iPod/) || navigator.userAgent.match(/iPad/)) {
         // fakeElem.innerText = "running iOS - you are running VoiceOver";
         return {
-            osVal: 0
-            , osTxt: "iOS"
+            osVal: 0,
+            osTxt: "iOS"
         };
-    }
-    else if (navigator.userAgent.match(/Macintosh/)) {
+    } else if (navigator.userAgent.match(/Macintosh/)) {
         // fakeElem.innerText = "running macOS";
         return {
-            osVal: 1
-            , osTxt: "macOS"
+            osVal: 1,
+            osTxt: "macOS"
         };
-    }
-    else if (navigator.userAgent.match(/Windows/)) {
+    } else if (navigator.userAgent.match(/Windows/)) {
         return {
-            osVal: 2
-            , osTxt: "Windows"
+            osVal: 2,
+            osTxt: "Windows"
         };
     }
 }
 /* Generate a screen reader score that determines the likelihood of a screen reader or other AT being used */
 var srScore = {
-    srBrowse: 0
-    , srFocus: 0
-    , kbd: 0
-    , fakeClicked: 0
-    , OS: detectPlatform()
-    , srTotal: function () {
+    srBrowse: 0,
+    srFocus: 0,
+    kbd: 0,
+    fakeClicked: 0,
+    OS: detectPlatform(),
+    srTotal: function () {
         return this.srBrowse + this.srFocus;
     }
 }
